0e26ba13106a7ed4c362653eba0b9eab4b331565
Angelina Kirpik <workangelinakirpik@gmail.com>
2026-05-26 20:35:40 +0300

    EmployeesChange

--- Employees/employees.sql
+++ Employees/employees.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Employees (
     employee_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL CHECK (name <> ''),
     department VARCHAR(100) NOT NULL,
-    manager_id INTEGER REFERENCES Employees(employee_id) ON DELETE SET NULL
+    manager_id INTEGER REFERENCES Employees(employee_id) ON DELETE SET NULL CONSTRAINT check_not_self_manager CHECK (employee_id != manager_id)
 );
